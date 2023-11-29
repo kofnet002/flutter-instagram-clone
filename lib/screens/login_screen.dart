92ff5672f36bfa2fa88cb79f42f37d3699a8e52a
Network <noeljoel61@gmail.com>
2023-11-29 09:55:53 +0000

    Able to authenticate and login user

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,7 +2,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram_clone/resources/auth_methods.dart';
 import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/utils/utils.dart';
 import 'package:instagram_clone/widgets/text_field_input.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -15,13 +17,32 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-
+  bool _isLoading = false;
 // to clear the controllers when done
   @override
   void dispose() {
     super.dispose();
     _emailController.dispose();
     _passwordController.dispose();
+  }
+
+  void loginUser() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    String res = await AuthMethods().loginUser(
+        email: _emailController.text, password: _passwordController.text);
+
+    setState(() {
+      _isLoading = false;
+    });
+
+    if (res == 'Login successful') {
+      showSnackBar('Logging in as ' + _emailController.text, context);
+    } else {
+      showSnackBar(res, context);
+    }
   }
 
   @override
@@ -73,8 +94,14 @@ class _LoginScreenState extends State<LoginScreen> {
 
             // login button
             InkWell(
+              onTap: loginUser,
               child: Container(
-                child: const Text('Log in'),
+                child: _isLoading
+                    ? Center(
+                        child: CircularProgressIndicator(
+                        color: primaryColor,
+                      ))
+                    : const Text('Log in'),
                 width: double.infinity,
                 alignment: Alignment.center,
                 padding: const EdgeInsets.symmetric(vertical: 12),
